c82b934712b60008d9a2213e9c1c3affd836ba20
Botiwa Konan <elibiekonan@gmail.com>
2021-07-05 03:27:09 -0700

    javascript update

---
+++ main.js
@@ -0,0 +1,44 @@
+const numberButtons = document.querySelectorAll('.value');
+const operationButtons = document.querySelectorAll('.operation');
+const mathProprieties = document.querySelectorAll('.math-function');
+const clearButton = querySelector('.clear');
+const deleteButton = querySelector('.deletion');
+const topScreen = document.querySelector('.top-output');
+const downScreen = document.querySelector('.bottom-output');
+
+class Calculator {
+  constructor(topScreen, downScreen) {
+    this.topScreen = topScreen;
+    this.downScreen = downScreen;
+  }
+  clear() {
+    this.topScreen.innerHTML = '';
+    this.downScreen.innerHTML = '';
+    this.operation = undefined;
+  }
+
+  appendNumbers(number) {
+    this.number = number;
+  }
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  display() {
+    this.topScreen.textContent = this.number;
+  }
+}
+
+const calculator = new Calculator(topScreen, downScreen);
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.appendNumbers(button.textContent);
+    calculator.display();
+  });
+});
+
+clearButton.addEventListener('click', () => {
+  calculator.clear();
+});
